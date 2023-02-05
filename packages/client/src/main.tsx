a1b256f2ecb1e2eb074bffeffee3c56a88c3ed20
Anton Litus <littox90@gmail.com>
2023-02-06 00:55:26 +0300

    COS-40: добавлен ErrorBoundary для обработки ошибок

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ErrorBoundary } from "./shared/utils/ErrorBoundary/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
