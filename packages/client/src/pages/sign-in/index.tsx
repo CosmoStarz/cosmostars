197542e9c0facec456c35d9af10970ad991ec9bd
dull69 <dull69@mail.ru>
2023-02-11 14:00:05 +0300

    COS-14: удалил лишнее поле

--- packages/client/src/pages/sign-in/index.tsx
+++ packages/client/src/pages/sign-in/index.tsx
@@ -1,12 +1,10 @@
-import { FC, PropsWithChildren } from "react";
 import { BasicLayout } from "../../shared/layouts/BasicLayout";
 import { SignIn } from "../../features/Auth/SignIn";
 
-export const SignInPage: FC<PropsWithChildren> = props => {
-    return (
-      <BasicLayout>
-        <SignIn />
-        {props.children}
-      </BasicLayout>
-    );
+export const SignInPage = () => {
+  return (
+    <BasicLayout>
+      <SignIn />
+    </BasicLayout>
+  );
 };
--- packages/client/src/pages/sign-up/index.tsx
+++ packages/client/src/pages/sign-up/index.tsx
@@ -1,12 +1,10 @@
-import { FC, PropsWithChildren } from "react";
 import { BasicLayout } from "../../shared/layouts/BasicLayout";
 import { SignUp } from "../../features/Auth/SignUp";
 
-export const SignUpPage: FC<PropsWithChildren> = props => {
-    return (
-      <BasicLayout>
-        <SignUp />
-        {props.children}
-      </BasicLayout>
-    );
+export const SignUpPage = () => {
+  return (
+    <BasicLayout>
+      <SignUp />
+    </BasicLayout>
+  );
 };
