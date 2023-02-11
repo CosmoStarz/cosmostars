ae56420bc6e31c5bb42aba280a854b693a007580
dull69 <dull69@mail.ru>
2023-02-11 14:03:51 +0300

    COS-14: линтинг, перевод в относительные единицы ширины карточки, поменял поле на field.name для добавления просмотра пароля

--- packages/client/src/shared/ui/Card/index.tsx
+++ packages/client/src/shared/ui/Card/index.tsx
@@ -1,139 +1,159 @@
-import * as React from 'react';
+import React, { useState, ChangeEvent, FocusEvent, ReactElement } from 'react';
 import {
-    Box, 
-    Card, 
-    CardHeader, 
-    CardContent, 
-    CardActions, 
-    Typography,
-    TextField, 
-    Button, 
-    Link } from '@mui/material';
-import Icon from "@mui/material/Icon";
-import InputAdornment from '@mui/material/InputAdornment';
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Typography,
+  TextField,
+  Button,
+  Link,
+} from "@mui/material";
 import IconButton from '@mui/material/IconButton';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { SvgIconProps } from '@mui/material';
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { SvgIconProps } from "@mui/material";
 
 
 type FieldsType = {
-    id: string;
-    label: string;
-    name: string;
-    value: string;
-    type: string;
-    onChange: (e: React.ChangeEvent<any>) => void;
-    onBlur: (e: any) => void;
-    error?: string;
-}
+  id: string;
+  label: string;
+  name: string;
+  value: string;
+  type: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (e: FocusEvent<HTMLInputElement>) => void;
+  error?: string;
+};
 type Props = {
-    className: string;
-    title?: string;
-    fields: FieldsType[];
-    buttonName?: string;
-    linkName?: string;
-    handleSubmit?: () => void;
-    icon?: SvgIconProps<'svg', any>;
+  className: string;
+  title?: string;
+  fields: FieldsType[];
+  buttonName?: string;
+  linkName?: string;
+  handleSubmit?: () => void;
+  icon?: ReactElement<SvgIconProps>;
 };
 
-const CardView = ({ className, title, fields, handleSubmit, buttonName, linkName, icon }: Props) => {
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-      };
-    return (
-        <Box className={className}
-        component="form" 
-        onSubmit={handleSubmit}
-        noValidate sx={{
-          width: "595px",
-          margin: "auto",
-          border: "1px solid",
-          background: "linear-gradient(152.97deg, rgba(0, 0, 0, 0.4655) 15.24%, rgba(0, 0, 0, 0.95) 115.24%) ",
-        }}>
-            <Card sx={{
-        mb: "2rem",
-        mt: "2rem",
-        background: "transparent",
+const CardView = ({
+  className,
+  title,
+  fields,
+  handleSubmit,
+  buttonName,
+  linkName,
+  icon,
+}: Props) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(show => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+  return (
+    <Box
+      className={className}
+      component="form"
+      onSubmit={handleSubmit}
+      noValidate
+      sx={{
+        maxWidth: "595px",
         width: "100%",
-        display: "flex",
+        margin: "auto",
+        border: "1px solid",
+        background:
+          "linear-gradient(152.97deg, rgba(0, 0, 0, 0.4655) 15.24%, rgba(0, 0, 0, 0.95) 115.24%) ",
+      }}>
+      <Card
+        sx={{
+          my: "2rem",
+          background: "transparent",
+          width: "100%",
+          display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-      }}>
-        { icon && <Box className="back-icon" component="div" 
-        sx={{
-        mt: "14px",
-        pl: "14px",
-        width: "100%"}}>
-            <Icon>{icon}</Icon>
-        </Box> }
+        }}>
+        {icon && (
+          <Box
+            className="back-icon"
+            sx={{
+              pl: "14px",
+              width: "100%",
+            }}>
+            <IconButton>{icon}</IconButton>
+          </Box>
+        )}
         <CardHeader
-        titleTypographyProps={{variant:'h3' }}
+          titleTypographyProps={{ variant: "h3" }}
           title={title}
           sx={{
-          textAlign: "center"
-        }}
+            padding: "0px",
+            textAlign: "center",
+          }}
         />
-        <Box sx={{
-          width: "492px",
-        }}>
-        <CardContent>
+        <Box
+          sx={{
+            width: "492px",
+          }}>
+          <CardContent>
             {fields.map(field => {
-                return <TextField 
-                margin="normal"
-                required
-                fullWidth
-                helperText={field.error}
-                id={field.id}
-                value={field.value}
-                label={field.label}
-                name={field.name}
-                type={showPassword ? 'text' : field.type}
-                onChange={field.onChange}
-                onBlur={field.onBlur}
-                key={field.id}
-                error={!!field.error}
-                InputProps={{
-                    endAdornment: field.type ==='password' && <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>,
-                  }}
-                ></TextField>
+              return (
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  helperText={field.error}
+                  id={field.id}
+                  value={field.value}
+                  label={field.label}
+                  name={field.name}
+                  type={showPassword ? "text" : field.type}
+                  onChange={field.onChange}
+                  onBlur={field.onBlur}
+                  key={field.id}
+                  error={!!field.error}
+                  InputProps={{
+                    endAdornment: field.name === "password" && (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}>
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}></TextField>
+              );
             })}
-        </CardContent>
-        <CardActions sx={{
-        display: "flex",
-        flexDirection: "column",
-          justifyContent: "center",
-      }}>
+          </CardContent>
+          <CardActions
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}>
             <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            >
-                {buttonName}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}>
+              {buttonName}
             </Button>
-            {linkName && <Link href="#">
-            <Typography>
-                {linkName}
-            </Typography>
-            </Link>}
-        </CardActions>
+            {linkName && (
+              <Link href="#">
+                <Typography>{linkName}</Typography>
+              </Link>
+            )}
+          </CardActions>
         </Box>
-        </Card>
-        
-        </Box>
-    );
+      </Card>
+    </Box>
+  );
 };
 
 export default CardView;
