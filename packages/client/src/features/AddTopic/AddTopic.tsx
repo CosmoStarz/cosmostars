b5416f1984b7cf7f95e16bef4daa33f6e0a53196
Anton Litus <littox90@gmail.com>
2023-02-14 07:25:26 +0300

    COS-17: перенос стилей модалки добавления топика

--- packages/client/src/features/AddTopic/AddTopic.tsx
+++ packages/client/src/features/AddTopic/AddTopic.tsx
@@ -30,61 +30,70 @@ export const AddTopic: FC = () => {
 
   return (
     <>
-    <Modal
-      open={open}
-      onClose={handleClose}
-    >
-      <Paper variant="outlined" className="modal">
-        <Typography id="modal-title" variant="h6" component="h3" align="center">
-          Add Topic
-        </Typography>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Paper variant="outlined"
+               sx={{
+                 position: "absolute",
+                 top: "50%",
+                 left: "50%",
+                 transform: "translate(-50%, -50%)",
+                 width: "400px",
+                 border: "2px solid #000",
+                 padding: "16px"
+               }}>
+          <Typography id="modal-title" variant="h6" component="h3" align="center">
+            Add Topic
+          </Typography>
 
-        <Box
-          component="form"
-          onSubmit={formikAddTopic.handleSubmit}
-          onReset={formikAddTopic.handleReset}
-          sx={{
-            width: "100%",
-            my: 2,
-            mr: 5
-          }}>
-          <TextField
-            fullWidth
-            id="topicName"
-            name="topicName"
-            label="Name"
-            onChange={formikAddTopic.handleChange}
-            error={formikAddTopic.touched.topicName && Boolean(formikAddTopic.errors.topicName)}
-            helperText={formikAddTopic.touched.topicName && formikAddTopic.errors.topicName}
+          <Box
+            component="form"
+            onSubmit={formikAddTopic.handleSubmit}
+            onReset={formikAddTopic.handleReset}
             sx={{
-              mb: 2
-            }}
-          />
-          <TextField
-            fullWidth
-            id="topicDescription"
-            name="topicDescription"
-            label="Description"
-            onChange={formikAddTopic.handleChange}
-            error={formikAddTopic.touched.topicDescription && Boolean(formikAddTopic.errors.topicDescription)}
-            helperText={formikAddTopic.touched.topicDescription && formikAddTopic.errors.topicDescription}
-            sx={{
-              mb: 2
-            }}
-          />
-          <Box component="div" sx={{
-            display: "flex",
-            justifyContent: "flex-end"
-          }}>
-            <Button variant="text" type="reset">Cancel</Button>
-            <Button variant="text" type="submit">Add</Button>
+              width: "100%",
+              my: 2,
+              mr: 5
+            }}>
+            <TextField
+              fullWidth
+              id="topicName"
+              name="topicName"
+              label="Name"
+              onChange={formikAddTopic.handleChange}
+              error={formikAddTopic.touched.topicName && Boolean(formikAddTopic.errors.topicName)}
+              helperText={formikAddTopic.touched.topicName && formikAddTopic.errors.topicName}
+              sx={{
+                mb: 2
+              }}
+            />
+            <TextField
+              fullWidth
+              id="topicDescription"
+              name="topicDescription"
+              label="Description"
+              onChange={formikAddTopic.handleChange}
+              error={formikAddTopic.touched.topicDescription && Boolean(formikAddTopic.errors.topicDescription)}
+              helperText={formikAddTopic.touched.topicDescription && formikAddTopic.errors.topicDescription}
+              sx={{
+                mb: 2
+              }}
+            />
+            <Box component="div" sx={{
+              display: "flex",
+              justifyContent: "flex-end"
+            }}>
+              <Button variant="text" type="reset">Cancel</Button>
+              <Button variant="text" type="submit">Add</Button>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
-    </Modal>
-    <Button variant="contained" size="large" type="button" onClick={handleOpen}>
-      Add +
-    </Button>
+        </Paper>
+      </Modal>
+      <Button variant="contained" size="large" type="button" onClick={handleOpen}>
+        Add +
+      </Button>
     </>
   );
 };
