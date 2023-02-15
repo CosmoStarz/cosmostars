393463f74b1d0dad190f94e77c94365fb1d8f2d8
Anton Litus <littox90@gmail.com>
2023-02-15 17:58:36 +0300

    COS-55: добавлена entity user в качестве примера

---
+++ packages/client/src/entities/user/model/types.d.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: number;
+  first_name: string;
+  second_name: string;
+  display_name: string;
+  login: string;
+  email: string;
+  phone: string;
+  avatar: string | null;
+}
