07abce854f8aaaf493423ba51881832e7e563f17
dull69 <dull69@mail.ru>
2023-02-21 19:25:55 +0300

    COS-56: добавил middleware

--- packages/client/src/app/store/index.ts
+++ packages/client/src/app/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authApi } from "../../entities/user/model";
+import { authApi } from "../../shared/api";
+import { authModel } from "../../entities/auth";
 
 export const store = configureStore({
   reducer: {
-    [authApi.reducerPath]: authApi.reducer,
+    [authApi.auth.authApi.reducerPath]: authApi.auth.authApi.reducer,
+    auth: authModel.authSlice.authReducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.auth.authApi.middleware),
   // todo: вынести в конфиг .env
   devTools: true,
 });
