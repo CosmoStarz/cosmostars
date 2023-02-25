2945d604a972e486a1624043c4003e72b93ea5eb
dull69 <dull69@mail.ru>
2023-02-25 22:18:59 +0300

    COS-56: вынес в отдельные константы

---
+++ packages/client/src/shared/constants/api.ts
@@ -0,0 +1,9 @@
+export const LS_IS_LOGGED_KEY = "isLogged";
+export const AUTH = "/auth/";
+export enum Method {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  PATCH = "PATCH",
+  DELETE = "DELETE",
+}
