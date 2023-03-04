a338322a5b30ec18ccde1353b5674f913f5435b9
dull69 <dull69@mail.ru>
2023-03-04 16:40:41 +0300

    COS-59: сделал hoc useAuth

--- packages/client/src/shared/hooks/useAuth.ts
+++ packages/client/src/shared/hooks/useAuth.ts
@@ -1,5 +1,56 @@
-import { useGetUserQuery } from "../api/auth/auth";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+import { useLazyGetUserQuery } from "@/entities/user/model/api";
+import { selectIsAuth, setIsAuth,setUser} from "@/entities/user/model/user";
+import { useLogoutMutation,useSignInMutation, useSignUpMutation } from "@/shared/api/auth/auth";
+
+import { SignInRequest, SignInResponse, SignUpRequest, SignUpResponse } from "../api/auth/models";
+import { useAppDispatch } from "./store";
+
 export const useAuth = () => {
-  const { data } = useGetUserQuery("user");
-  return !!data;
-};
+  const dispatch = useAppDispatch();
+  const isAuth = useSelector(selectIsAuth);
+  const [isLoadingAuth, setIsLoadingAuth] = useState(false);
+
+  const [getUser] = useLazyGetUserQuery();
+  const [signIn] = useSignInMutation();
+  const [signUp] = useSignUpMutation();
+  const [logout] = useLogoutMutation();
+
+  const checkIsUserAuth = async () => {
+    setIsLoadingAuth(true);
+    try {
+      const { isSuccess,data } = await getUser();
+      dispatch(setIsAuth(isSuccess));
+      if (isSuccess) {
+        dispatch(setUser(data));
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoadingAuth(false);
+    }
+  }
+  const signInAuth = async (userForm: SignInRequest) => {
+    const data = await signIn(userForm);
+    if ((data as unknown as SignInResponse).error.data === "OK") {
+      checkIsUserAuth()
+    }
+  }
+
+  const signUpAuth = async (userForm: SignUpRequest) => {
+    const response = await signUp(userForm);
+    // приводится к типу unknown, т.к в базовых типах нет поле data
+    // из-за этого TS выдает ошибку
+    if ((response as unknown as SignUpResponse).data.id) {
+      checkIsUserAuth()
+    }
+  };
+
+  const logoutAuth = async () => {
+    await logout("");
+    await checkIsUserAuth()
+  };
+  return { logoutAuth, signUpAuth, signInAuth, checkIsUserAuth, isLoadingAuth, isAuth };
+}
