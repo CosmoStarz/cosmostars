7eb06691b14bd7bf4a8bc56aefcb7f1e3b62c08f
dull69 <dull69@mail.ru>
2023-03-08 13:14:14 +0300

    COS-74: линтер

--- packages/client/src/widgets/MainMenu/MainMenu.tsx
+++ packages/client/src/widgets/MainMenu/MainMenu.tsx
@@ -7,20 +7,20 @@ import { ThemeToggler } from "@/features/ThemeToggler/ThemeToggler";
 import { MenuItems } from "@/shared/constants";
 
 const paperStyleBase = {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      background:
-          "linear-gradient(126.97deg, rgba(6, 11, 40, 0.26) 28.26%, rgba(10, 14, 35, 0.42) 91.2%)",
-      backdropFilter: "blur(21px)",
-      borderRadius: 3,
-}
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  background:
+    "linear-gradient(126.97deg, rgba(6, 11, 40, 0.26) 28.26%, rgba(10, 14, 35, 0.42) 91.2%)",
+  backdropFilter: "blur(21px)",
+  borderRadius: 3,
+};
 const paperStyleActions = {
   ...paperStyleBase,
   position: "absolute",
   right: "2%",
   height: "100%",
-}
+};
 export const MainMenu: FC = () => {
   return (
     <Box
@@ -32,26 +32,22 @@ export const MainMenu: FC = () => {
         position: "relative",
         justifyContent: "center",
       }}>
-        <Paper
-          variant="outlined"
-          sx={paperStyleBase}>
-          <List
-            sx={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-            }}>
-            {MenuItems.map((item, index) => (
-              <MenuLink link={item} key={index} />
-            ))}
-          </List>
-        </Paper>
-        <Paper
-          variant="outlined"
-          sx={paperStyleActions}>
-          <ThemeToggler />
-          <Logout />
-        </Paper>
+      <Paper variant="outlined" sx={paperStyleBase}>
+        <List
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}>
+          {MenuItems.map((item, index) => (
+            <MenuLink link={item} key={index} />
+          ))}
+        </List>
+      </Paper>
+      <Paper variant="outlined" sx={paperStyleActions}>
+        <ThemeToggler />
+        <Logout />
+      </Paper>
     </Box>
   );
 };
