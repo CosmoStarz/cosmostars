3d1122de1252509cc557635b8653f9a703e05f32
Alexander <legionerab3000@yandex.ru>
2023-04-01 12:45:05 +0000

    Connect add comment endpoint to UI

--- packages/client/src/entities/notification/config.ts
+++ packages/client/src/entities/notification/config.ts
@@ -1,6 +1,8 @@
 export const actions = [
   "yandexApi/executeQuery/rejected",
   "yandexApi/executeMutation/rejected",
+  "internalApi/executeQuery/rejected",
+  "internalApi/executeMutation/rejected",
 ];
 
 export const DEFAULT_ERROR_TEXT = "Unexpected error";
