270bdd26c5b5061db5a6ea8413740d065b32f764
AGromkova <AGromkova@ibs.root.ibscorp.ru>
2023-04-02 10:39:34 +0300

    add endpoint to contants

--- packages/client/src/entities/theme/api.ts
+++ packages/client/src/entities/theme/api.ts
@@ -1,6 +1,7 @@
 import { internalApi } from "@/shared/api/internalApi";
 import { HTTPMethods, InternalTags } from "@/shared/api/types";
 import { getErrorReason } from "@/shared/api/utils";
+import { THEME_ENDPOINT } from "@/shared/constants/api";
 
 import { ThemeCreationData, ThemeData } from "./types";
 
@@ -8,14 +9,14 @@ const themeApi = internalApi.injectEndpoints({
   endpoints: builder => ({
     getUserTheme: builder.query<ThemeData, number>({
       query: (id: number) => ({
-        url: `/theme/${id}`,
+        url: `/${THEME_ENDPOINT}/${id}`,
         method: HTTPMethods.GET,
       }),
       providesTags: [InternalTags.THEME],
     }),
     postUserTheme: builder.mutation<ThemeData, ThemeCreationData>({
       query: ({ id, theme }: ThemeCreationData) => ({
-        url: `/theme/${id}`,
+        url: `/${THEME_ENDPOINT}/${id}`,
         method: HTTPMethods.POST,
         body: { theme },
       }),
