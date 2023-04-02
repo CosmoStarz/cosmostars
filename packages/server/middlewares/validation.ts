998eb807da557330a061ea70d994a0f893044aac
Anton Litus <littox90@gmail.com>
2023-04-03 00:08:21 +0300

    COS-83: валидация базовая

--- packages/server/middlewares/validation.ts
+++ packages/server/middlewares/validation.ts
@@ -1,8 +1,12 @@
 import type express from "express";
-import { ValidationChain,validationResult } from 'express-validator';
+import { ValidationChain, validationResult } from "express-validator";
 
 export const validate = (validations: ValidationChain[]) => {
-  return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  return async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
     await Promise.all(validations.map(validation => validation.run(req)));
 
     const errors = validationResult(req);
--- packages/server/routes/TopicRoutes.ts
+++ packages/server/routes/TopicRoutes.ts
@@ -10,11 +10,15 @@ import { validate } from "../middlewares/validation";
 
 const router = Router();
 
-router.post("/", validate([
-  body('title', 'title - минимум 2 символа').isLength({min: 2}),
-  body('description', 'description - минимум 2 символа').isLength({min: 2}),
-  body('author_id', 'author_id - только числовой id').exists().isNumeric(),
-]), createTopic);
+router.post(
+  "/",
+  validate([
+    body("title", "title - минимум 2 символа").isLength({ min: 2 }),
+    body("description", "description - минимум 2 символа").isLength({ min: 2 }),
+    body("author_id", "author_id - только числовой id").exists().isNumeric(),
+  ]),
+  createTopic
+);
 
 router.get("/", getAllTopic);
 
