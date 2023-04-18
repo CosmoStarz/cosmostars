c0713cb26c55aca16ef32cad8b0574c6c0dffc07
Anton Litus <littox90@gmail.com>
2023-04-18 21:53:41 +0300

    COS-114: добавлен author в res где он необходим

--- packages/server/controllers/CommentController.ts
+++ packages/server/controllers/CommentController.ts
@@ -3,9 +3,15 @@ import type { RequestHandler } from "express";
 import { BaseStatuses } from "../constants";
 import { Comment } from "../db/models/Comment";
 import { Like } from "../db/models/Like";
+import { User } from "../db/models/User";
 
 export const createComment: RequestHandler = async (req, res) => {
-  const comment = await Comment.create({ ...req.body });
+  // Comment.belongsTo(User);
+  const comment = await Comment.create({
+    ...req.body,
+    author_id: req.user.ya_id,
+  });
+  await comment.reload({ include: User });
   return res.status(BaseStatuses.CREATED).json(comment);
 };
 
@@ -13,14 +19,20 @@ export const getByTopicId: RequestHandler = async (req, res) => {
   const { id } = req.params;
   const comments: Comment[] = await Comment.findAll({
     raw: true,
-    include: {
-      model: Like,
-      attributes: ["user_id"],
-      where: {
-        user_id: req.user.id,
+    nest: true,
+    include: [
+      {
+        model: Like,
+        attributes: ["user_id"],
+        where: {
+          user_id: req.user.ya_id,
+        },
+        required: false,
       },
-      required: false,
-    },
+      {
+        model: User,
+      },
+    ],
     where: { topic_id: id },
     order: [
       ["id", "ASC"],
--- packages/server/controllers/TopicController.ts
+++ packages/server/controllers/TopicController.ts
@@ -2,15 +2,17 @@ import type { RequestHandler } from "express";
 
 import { BaseStatuses, ErrorMessages } from "../constants";
 import { Topic } from "../db/models/Topic";
+import { User } from "../db/models/User";
 import { configureError } from "../utils/configureError";
 
 export const createTopic: RequestHandler = async (req, res) => {
-  const topic = await Topic.create({ ...req.body });
+  const topic = await Topic.create({ ...req.body, author_id: req.user.ya_id });
+  await topic.reload({ include: User });
   return res.status(BaseStatuses.CREATED).json(topic);
 };
 
 export const getAllTopic: RequestHandler = async (_req, res) => {
-  const allTopics: Topic[] = await Topic.findAll();
+  const allTopics: Topic[] = await Topic.findAll({ include: User });
   const topicCount: number = await Topic.count();
   return res
     .status(BaseStatuses.OK)
@@ -19,7 +21,9 @@ export const getAllTopic: RequestHandler = async (_req, res) => {
 
 export const getTopicById: RequestHandler = async (req, res) => {
   const { id } = req.params;
-  const topic: Topic | null = await Topic.findByPk(isNaN(+id) ? 0 : id);
+  const topic: Topic | null = await Topic.findByPk(isNaN(+id) ? 0 : id, {
+    include: User,
+  });
   if (topic == null) {
     return configureError(res, BaseStatuses.NOT_FOUND, ErrorMessages.NOT_FOUND);
   }
